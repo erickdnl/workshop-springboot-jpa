69eab886e8b1507526e773f846093c4de1cc899e
Erick Daniel <derickanielgg@gmail.com>
2025-01-08 19:48:33 -0300

    Project created

---
+++ src/test/java/com/example/sete/SeteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
