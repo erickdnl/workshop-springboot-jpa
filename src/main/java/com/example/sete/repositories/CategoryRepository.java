d202598e9bc12e01240461851e9d7c742fa3cb93
Erick Daniel <derickanielgg@gmail.com>
2025-01-13 19:16:15 -0300

    Category

---
+++ src/main/java/com/example/sete/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.example.sete.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.sete.entities.Category;
+
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
